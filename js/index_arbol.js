c61d6a48dd8db76072ad2b2048a30c473a8c9d16
Dartron <leorodriguez.0150@gmail.com>
2023-01-18 23:44:10 -0300

    Actualizacion arbol

---
+++ js/index_arbol.js
@@ -0,0 +1,6 @@
+function iniciarMusica(){
+    console.log("hola");
+    var audio = document.getElementById("musica");
+    audio.play();
+    document.getElementById("contenedor_musica").style.display = "none";
+}
